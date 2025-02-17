ebce2a7bb94f339290f7a9226ede61da4f2f83d8
Jong Myoung <djm342@naver.com>
2025-02-17 09:43:05 +0900

    v0.2 day06 bit_operation

--- day06.py
+++ day06.py
@@ -9,6 +9,15 @@ def is_even(number):
     else:
         return False
 
-number = int(input())
+num = int(input())
 
-print(is_even(number))
+print(is_even(num))
+
+#bit operation
+
+a = 10 # 0000 1010
+b = 11 # 0000 1011
+
+print(a & b) # 0000 1010 <- &연산은 둘다 1일때만 1 나머지 경우엔 전부 0
+print(a | b) # 0000 1011 <- | 하나라도 1이면 1
+print(a ^ b) #0000 0001 <- ^는 값이 서로 다를때 1 아니면 0
