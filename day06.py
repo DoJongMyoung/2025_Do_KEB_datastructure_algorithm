42304e1905b476ceee45a9592dc489477b9f9171
Jong Myoung <djm342@naver.com>
2025-02-17 14:55:51 +0900

    v1.1 recurison memoization

--- day06.py
+++ day06.py
@@ -1,34 +1,45 @@
-def print_poly(f_x, t_x) -> str:
-    poly_expression = "f(x) = "
+# def pbo(n):
+#     n_list=[0 ,1]
+#     for i in range(n+1):
+#         n_list.append(n_list[i] + n_list[i + 1])
+#
+#     return n_list[n]
+#
+#
+# number = int(input("보고 싶은 n번째 피보나치 숫자: "))
+# print(pbo(number))
 
-    for i in range(len(fx)):
-        coefficient = f_x[i]
-        term = t_x[i]
+def fibonacci_recursion(n) -> int:
+    """
+    재귀 함수를 이용한 피보나치 계산
+    :param n:
+    :return: int타입의 피보나치 값
+    """
+    if n<=0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci_recursion(n-2) + fibonacci_recursion(n-1)
 
-        if coefficient >= 0 and i != 0:
-            poly_expression = poly_expression + "+"
-        poly_expression = poly_expression + f'{coefficient}x^{term} '
+print(fibonacci_recursion(int(input())))
 
-    return poly_expression
+#시간 복잡도 계산 낮추는 대신 공간 복잡도를 높이는 방법 .
 
+memo = dict()
 
-def calculation_poly(x_value, f_x, t_x) -> int:
-    return_value = 0
+def dp_fibonacci_recursion(n) -> int:
+    """
+    재귀 함수를 이용한 피보나치 계산
+    :param n:
+    :return: int타입의 피보나치 값
+    """
+    if n in memo: #딕셔너리에 이미 계산된 결과가 있으면 그값을 리턴
+        return memo[n]
+    elif n<=1: # 0 또는 1이 오면 그 값을 바로 리턴
+        return n
+    else:
+        memo[n] = dp_fibonacci_recursion(n-2) + dp_fibonacci_recursion(n-1)
+        return memo[n]
 
-    for i in range(len(fx)):
-        coefficient = f_x[i]
-        term = t_x[i]
-        return_value += coefficient * pow(x_value, term)
-
-    return return_value
-
-
-fx = [2, 5 ,-9 , 11]
-
-tx = [20, 7, 2, 0]
-
-
-if __name__ == "__main__":
-    print(print_poly(fx,tx))
-    print(calculation_poly(int(input("x 값 : ")), fx, tx))
-
+print(dp_fibonacci_recursion(int(input())))
