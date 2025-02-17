3a64c776560648af54739b7a1ce6cc855f63c377
Jong Myoung <djm342@naver.com>
2025-02-17 14:15:30 +0900

    v1.0 real fixed ver

--- day06.py
+++ day06.py
@@ -1,12 +1,11 @@
 def print_poly(f_x, t_x) -> str:
-    # term = len(f_x) - 1
     poly_expression = "f(x) = "
 
     for i in range(len(fx)):
         coefficient = f_x[i]
         term = t_x[i]
 
-        if coefficient >= 0:
+        if coefficient >= 0 and i != 0:
             poly_expression = poly_expression + "+"
         poly_expression = poly_expression + f'{coefficient}x^{term} '
 
@@ -15,13 +14,11 @@ def print_poly(f_x, t_x) -> str:
 
 def calculation_poly(x_value, f_x, t_x) -> int:
     return_value = 0
-    # term = len(f_x) - 1
 
     for i in range(len(fx)):
         coefficient = f_x[i]
         term = t_x[i]
         return_value += coefficient * pow(x_value, term)
-        # term = term - 1
 
     return return_value
 
