62bdd4ad1d62b7632c366664007cbf969d4cf5e5
Jong Myoung <djm342@naver.com>
2025-02-17 17:05:16 +0900

    v1.4 CSV

--- day06.py
+++ day06.py
@@ -10,16 +10,23 @@
 #
 # print(list)
 
+import csv
+import random
+
 students = []
 
 try:
-    file = input("File name : ")
-    fp = open(file, 'r')
-    readme_list = fp.readlines()
-    rls = readme_list[0].split("_")
-    print(readme_list)
-    print(rls)
-    fp.close()
+    with open("students.csv", 'r') as fp:
+        students_list = fp.readlines()
+        print(students_list)
+        students_list.remove("이상혁\n")
+        students_list.remove("김찬빈\n")
+        students_list.remove("김철중\n")
+
+        for _ in range(3):
+            random_pick = random.choice(students_list)
+            print(random_pick,end='')
+            students_list.remove(random_pick)
 
 except FileNotFoundError as err: #여기서 err은 에러 메세지를 받는 변수명 , 다른거 써도 무관
-    print(f"{file} is not exist . {err}")
+    print(err)
