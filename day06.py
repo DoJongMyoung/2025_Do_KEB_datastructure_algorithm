1c32aa2f547f89eaaae5604a4e777e95abfb1256
Jong Myoung <djm342@naver.com>
2025-02-17 10:41:26 +0900

    v0.4 day06 conversion

--- day06.py
+++ day06.py
@@ -1,14 +1,27 @@
-def is_even(number):
-    """
-    짝수 판정 함수
-    :param number: 판정할 함수
-    :return: 짝수면 True, 홀수면 False
-    """
+#숫자를 입력했을때 8진수 결과를 내어놓는 프로그램 작성하기
 
-    return not number & 1
-    #return number & 1 # 짝수 넣으면 0반환 홀수 넣으면 1반환
+def dec_oct(n):
+    if n == 0:
+        return ""
+    else:
+        return dec_oct(n // 8) + str(n % 8) # //는 몫을 나타냄. %는 나머지
+
+n = int(input())
+print(dec_oct(n))
 
 
-num = int(input())
-
-print(is_even(num))
+# def is_even(number):
+#     """
+#     짝수 판정 함수
+#     :param number: 판정할 함수
+#     :return: 짝수면 True, 홀수면 False
+#     """
+#
+#     return not number & 1
+#     #return number & 1 # 짝수 넣으면 0반환 홀수 넣으면 1반환
+#
+#
+# num = int(input())
+#
+# print(is_even(num))
+#
