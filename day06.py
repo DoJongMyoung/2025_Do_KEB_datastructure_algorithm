5a88eb2c1a74c3ef09213bce097c2371647a2af8
Jong Myoung <djm342@naver.com>
2025-02-17 09:51:55 +0900

    v0.3 day06 is_even_updated

--- day06.py
+++ day06.py
@@ -4,20 +4,11 @@ def is_even(number):
     :param number: 판정할 함수
     :return: 짝수면 True, 홀수면 False
     """
-    if number % 2 == 0:
-        return True
-    else:
-        return False
+
+    return not number & 1
+    #return number & 1 # 짝수 넣으면 0반환 홀수 넣으면 1반환
+
 
 num = int(input())
 
 print(is_even(num))
-
-#bit operation
-
-a = 10 # 0000 1010
-b = 11 # 0000 1011
-
-print(a & b) # 0000 1010 <- &연산은 둘다 1일때만 1 나머지 경우엔 전부 0
-print(a | b) # 0000 1011 <- | 하나라도 1이면 1
-print(a ^ b) #0000 0001 <- ^는 값이 서로 다를때 1 아니면 0
