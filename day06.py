f9e9989ed9444a6df041ab7ab3ee32b1d7ef114e
Jong Myoung <djm342@naver.com>
2025-02-17 18:58:55 +0900

    v1.6 assignment

--- day06.py
+++ day06.py
@@ -1,32 +1,35 @@
-# list = [20,30,40,40,50,80,80,80,90]
-#
-# print(list)
-#
-# list.pop(4)
-#
-# print(list)
-#
-# list.remove(80)
-#
-# print(list)
-
-import csv
 import random
 
-students = []
+def guess_number(low, high, answer, chance) -> int:
+    mid =  (low+high) // 2
+    print(f'Guess number is {mid}')
+    fp.write(f'Guess number is {mid}\n')
 
-try:
-    with open("students.csv", 'r') as fp:
-        students_list = fp.readlines()
-        print(students_list)
-        students_list.remove("이상혁\n")
-        students_list.remove("김찬빈\n")
-        students_list.remove("김철중\n")
 
-        for _ in range(3):
-            random_pick = random.choice(students_list)
-            print(random_pick,end='')
-            students_list.remove(random_pick)
+    while chance != 0:
+        if mid == answer:
+            print(f'You win. Answer is {answer}')
+            fp.write(f'You win. Answer is {answer}\n')
+            return
+        elif mid > answer:
+            chance = chance - 1
+            print(f'{mid} is bigger. Chance left : {chance}')
+            fp.write(f'{mid} is bigger. Chance left : {chance}\n')
+            return guess_number(low, mid, answer, chance) #mid가 크므로 상한에 mid를 배정, mid-1을 하는 이유는 어차피 mid가 답이 아니므로 상한을 -1해도 상관없음
+        else:
+            chance = chance - 1
+            print(f'{mid} is lower. Chance left : {chance}')
+            fp.write(f'{mid} is lower. Chance left : {chance}\n')
+            return guess_number(mid, high, answer, chance)
+    else:
+        print(f'You lost. Answer is {answer}')
+        fp.write(f'You lost. Answer is {answer}')
 
-except FileNotFoundError as err: #여기서 err은 에러 메세지를 받는 변수명 , 다른거 써도 무관
-    print(err)
+
+if __name__ == "__main__":
+    low = 1
+    high = 100
+    chance = 7
+    answer = random.randint(low, high)
+    with open('guess.txt', 'w') as fp:
+        guess_number(low, high, answer, chance)
