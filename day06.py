e13e7c784a83e932b39820472c550f5d5017dabe
Jong Myoung <djm342@naver.com>
2025-02-17 15:56:46 +0900

    v1.2 array

--- day06.py
+++ day06.py
@@ -1,45 +1,8 @@
-# def pbo(n):
-#     n_list=[0 ,1]
-#     for i in range(n+1):
-#         n_list.append(n_list[i] + n_list[i + 1])
-#
-#     return n_list[n]
-#
-#
-# number = int(input("보고 싶은 n번째 피보나치 숫자: "))
-# print(pbo(number))
 
-def fibonacci_recursion(n) -> int:
-    """
-    재귀 함수를 이용한 피보나치 계산
-    :param n:
-    :return: int타입의 피보나치 값
-    """
-    if n<=0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fibonacci_recursion(n-2) + fibonacci_recursion(n-1)
+a = [9 ,47 ,88 , -2]
 
-print(fibonacci_recursion(int(input())))
+print(id(a[1]), id([3]))
+#C나 Cpp에서
+# int a[4] = [9, -4, 78, 66]
+#cout << &a[1] << "/" << &a[3] 이런식으로 출력해보면 정확히 둘의 주소가 8차이 남.
 
-#시간 복잡도 계산 낮추는 대신 공간 복잡도를 높이는 방법 .
-
-memo = dict()
-
-def dp_fibonacci_recursion(n) -> int:
-    """
-    재귀 함수를 이용한 피보나치 계산
-    :param n:
-    :return: int타입의 피보나치 값
-    """
-    if n in memo: #딕셔너리에 이미 계산된 결과가 있으면 그값을 리턴
-        return memo[n]
-    elif n<=1: # 0 또는 1이 오면 그 값을 바로 리턴
-        return n
-    else:
-        memo[n] = dp_fibonacci_recursion(n-2) + dp_fibonacci_recursion(n-1)
-        return memo[n]
-
-print(dp_fibonacci_recursion(int(input())))
