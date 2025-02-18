a3ca5929b875be3226317b285d110ec6fa13c494
Jong Myoung <djm342@naver.com>
2025-02-18 13:45:33 +0900

    v2.4 remove_added

--- day07.py
+++ day07.py
@@ -1,27 +1,26 @@
 import random
 
+
 class Node:
-    def __init__(self,data,next=None):
+    def __init__(self, data, next=None):
         self.data = data
         self.next = next
+
 
 class LinkedList:
     def __init__(self):
         self.head = None
 
-    def append(self,data):
-        if not self.head: #head가 none이면 F반환 하는데 not이 있으므로 T반환. 따라서 head가 none이면 if문 실행
+    def append(self, data):
+        if not self.head:
             self.head = Node(data)
             return
-
-        current = self.head #current라는 지역변수에 head를 저장.
-        while current.next: #current가 가르치고 있는것이 존재하면
-            current = current.next
-
+        current = self.head
+        while current.next:  # if next node exist
+            current = current.next  # move
         current.next = Node(data)
 
-
-    def search(self,target) -> bool:
+    def search(self, target) -> bool:
         current = self.head
         while current.next:
             if current.data == target:
@@ -30,6 +29,20 @@ class LinkedList:
                 current = current.next
         return False
 
+    def remove(self, target):
+        if self.head.data == target:  # !
+            self.head = self.head.next  # head update
+            print("!")
+            return
+        current = self.head
+        privious = None
+        while current:
+            if current.data == target:
+                privious.next = current.next
+                break  # !
+            else:  # move
+                privious = current
+                current = current.next
 
     def __str__(self):
         node = self.head
@@ -42,11 +55,21 @@ class LinkedList:
 
 if __name__ == "__main__":
     l = LinkedList()
-    i = 0
-    while i < 20:
-        n = random.randint(1,20)
-        l.append(n)
-        print(n, end = " ")
-        i = i + 1
-    # print(l)
-    print(l.search(10))
+    l.append(7)
+    l.append(-11)
+    l.append(8)
+    print(l)
+    # l.remove(7)
+    # l.remove(-11)
+    l.remove(8)
+    print(l)
+
+    # l = LinkedList()
+    # i = 0
+    # while i < 20:
+    #     n = random.randint(1, 20)
+    #     l.append(n)
+    #     print(n, end=' ')
+    #     i = i + 1
+    # #print(l)
+    # print(l.search(10))
