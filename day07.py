5f372fee75671f386189c17eef815ae767ee01ce
Jong Myoung <djm342@naver.com>
2025-02-18 11:35:50 +0900

    v2.3 random_added

--- day07.py
+++ day07.py
@@ -1,3 +1,5 @@
+import random
+
 class Node:
     def __init__(self,data,next=None):
         self.data = data
@@ -40,8 +42,11 @@ class LinkedList:
 
 if __name__ == "__main__":
     l = LinkedList()
-    l.append(7)
-    l.append(-11)
-    l.append(8)
-    print(l)
-    # print(l) <-  __str__ 없을때 이렇게 하면 주소가 나옴
+    i = 0
+    while i < 20:
+        n = random.randint(1,20)
+        l.append(n)
+        print(n, end = " ")
+        i = i + 1
+    # print(l)
+    print(l.search(10))
