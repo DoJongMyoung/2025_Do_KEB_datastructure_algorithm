785d879e248cdd3910d844e8cbc83ec35f1054a0
Jong Myoung <djm342@naver.com>
2025-02-18 11:02:06 +0900

    v1.9 node head linked_list

--- day07.py
+++ day07.py
@@ -1,18 +1,26 @@
-def a(i):
-    j = 9
-    print(i , j)
+class Node:
+    def __init__(self,data,next=None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self,data):
+        if not self.head: #head가 none이면 F반환 하는데 not이 있으므로 T반환. 따라서 head가 none이면 if문 실행
+            self.head = Node(data)
+            return
+
+        current = self.head #current라는 지역변수에 head를 저장.
+        while current.next: #current가 가르치고 있는것이 존재하면
+            current = current.next
+
+        current.next = Node(data)
 
 
-def b(k):
-    a(1)
-    print(k)
-
-
-def main():
-    print("start")
-    b(3)
-    print("end")
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == "main":
+    l = LinkedList()
+    l.append(7)
+    l.append(-11)
+    l.append(8)
