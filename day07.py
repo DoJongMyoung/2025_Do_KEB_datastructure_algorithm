b2c9ffc98cd081857711174b1fa2b6c6a3b57c88
Jong Myoung <djm342@naver.com>
2025-02-18 10:10:20 +0900

    v1.8 stack debugger

--- day07.py
+++ day07.py
@@ -1,33 +1,18 @@
-class Stack:
-    def __init__(self):
-        self.items = []
-
-    def push(self,data):
-        self.items.append(data)
-
-    def pop(self):
-        return self.items.pop()
-
-    def size(self):
-        return len(self.items)
-
-    def is_empty(self):
-        return len(self.items) == 0
-
-    def peek(self):
-        return self.items[-1]
-
-    def is_empty(self) -> bool:
-        return len(self.items) == 0
+def a(i):
+    j = 9
+    print(i , j)
 
 
+def b(k):
+    a(1)
+    print(k)
 
-s1 = Stack()
+
+def main():
+    print("start")
+    b(3)
+    print("end")
 
 
-s1.push(-9)
-s1.push(11)
-s1.push(977)
-print(s1.pop()) #pop은 리턴 후 삭제
-print(s1.peek()) #peek은 리턴만.
-print(s1.peek())
+if __name__ == "__main__":
+    main()
