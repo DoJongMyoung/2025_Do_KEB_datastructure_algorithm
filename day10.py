04f90440bdb6ef552e2b7fcfb9da474a4faa2385
Jong Myoung <djm342@naver.com>
2025-02-21 10:28:00 +0900

    v4.7 bubble_sort2

--- day10.py
+++ day10.py
@@ -1,9 +1,15 @@
 def bubble_sort(l):
     list_length = len(l) - 1
     for i in range(list_length):
-        for j in range(list_length):
+        no_swap = True
+        for j in range(list_length - i): # !
             if l[j] > l[j+1]:
                 l[j] , l[j + 1] = l[j + 1], l[j]
+                no_swap = False
+                print(j, end = " ")
+        if no_swap:
+            return l
     return l
 
-print(bubble_sort([7,-11,66,4]))
+print(bubble_sort([33,8,-11,1]))
+print(bubble_sort([11,13,15,19]))
