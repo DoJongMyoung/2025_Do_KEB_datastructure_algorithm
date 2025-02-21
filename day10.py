d2dec2301e59af33c1d719d1831c6f1f61c3e068
Jong Myoung <djm342@naver.com>
2025-02-21 11:20:49 +0900

    v4.8 insertion_sort

--- day10.py
+++ day10.py
@@ -1,3 +1,14 @@
+def insertion_sort(l):
+    for i in range(1,len(l)):
+        value = l[i]
+        while i > 0 and l[i - 1] > value:
+            l[i] = l[i-1]
+            i = i - 1
+            print(i , end = " ")
+        l[i] = value
+    return l
+
+
 def bubble_sort(l):
     list_length = len(l) - 1
     for i in range(list_length):
@@ -13,3 +24,6 @@ def bubble_sort(l):
 
 print(bubble_sort([33,8,-11,1]))
 print(bubble_sort([11,13,15,19]))
+
+print(insertion_sort([33,8,-11,1]))
+print(insertion_sort([11,13,15,19]))
